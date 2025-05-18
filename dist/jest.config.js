8219f60d64a735ba0bc77686bdf017e63fd0725e
김용민 <dydals3440@gmail.com>
2025-05-18 23:23:24 +0900

    feat: TypeScript 기반 Enum 유틸리티 패키지 구현

--- dist/jest.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/test/**/*.spec.ts'],
-};
-exports.default = config;
